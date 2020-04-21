549c86a17edffa4add1a144b434539f96bbd12ed
blackd0t <blackd0t@protonmail.com>
2020-04-21 09:20:12 +0300

    Prinitng letter c

---
+++ src/15SevenSegmentDisplay/Single/02print_letter_c/02print_letter_c.ino
@@ -0,0 +1,34 @@
+int i, j;
+
+void setup() {    
+    // Setting all the pins connected to be OUTPUT PINS          
+    for(i = 2; i <= 8; i++){
+        pinMode(i, OUTPUT);
+    }
+}
+
+void turnOff(){
+    // Turn of all LED's
+    for(i = 2; i <= 8; i++){
+        digitalWrite(i, LOW);
+    }
+}
+
+void displayDigit(){
+    /*
+        a(2)
+    f(7)       b(3)
+        g(8)
+    e(6)        c(4)
+        d(5)
+    */
+   digitalWrite(2, HIGH);
+   digitalWrite(7, HIGH);
+   digitalWrite(6, HIGH);
+   digitalWrite(5, HIGH);
+}
+
+void loop() {
+    turnOff();
+    displayDigit();
+}
